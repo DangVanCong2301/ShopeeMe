52dc1eab7eb9beaa27191fcdbf2a1387c0a74d9f
dangvancong2301@gmail.com <dangvancong2301@gmail.com>
2024-09-23 16:33:30 +0700

    edit ui shop page

---
+++ wwwroot/js/site_shop.js
@@ -0,0 +1,109 @@
+// Load Product
+function loadingProducts() {
+    const loadingProductImage = document.querySelectorAll(".home-product-item__img-loading");
+    const loadingProductName = document.querySelectorAll(".home-product-item__name-loading");
+    const loadingProductPriceOld = document.querySelectorAll(".home-product-item__price-old-loading");
+    const loadingProductPriceCurrent = document.querySelectorAll(".home-product-item__price-current-loading");
+    const loadingProductLike = document.querySelectorAll(".home-product-item__like-loading");
+    const loadingProductRate = document.querySelectorAll(".home-product-item__rating-loading");
+    const loadingProductSold = document.querySelectorAll(".home-product-item__sold-loading");
+    const loadingProductBrand = document.querySelectorAll(".home-product-item__brand-loading");
+    const loadingProductOrigin = document.querySelectorAll(".home-product-item__origin-name-loading");
+
+    setTimeout(() => {
+        for (let i = 0; i < loadingProductImage.length; i++) {
+            loadingProductImage[i].style.display = 'none';
+        }
+        for (let i = 0; i < loadingProductName.length; i++) {
+            loadingProductName[i].style.display = 'none';
+        }
+        for (let i = 0; i < loadingProductPriceOld.length; i++) {
+            loadingProductPriceOld[i].style.display = 'none';
+        }
+        for (let i = 0; i < loadingProductPriceCurrent.length; i++) {
+            loadingProductPriceCurrent[i].style.display = 'none';
+        } 
+        for (let i = 0; i < loadingProductLike.length; i++) {
+            loadingProductLike[i].style.display = 'none';
+        }
+        for (let i = 0; i < loadingProductRate.length; i++) {
+            loadingProductRate[i].style.display = 'none';
+        }
+        for (let i = 0; i < loadingProductSold.length; i++) {
+            loadingProductSold[i].style.display = 'none';
+        }
+        for (let i = 0; i < loadingProductBrand.length; i++) {
+            loadingProductBrand[i].style.display = 'none';
+        }
+        for (let i = 0; i < loadingProductOrigin.length; i++) {
+            loadingProductOrigin[i].style.display = 'none';
+        }
+    }, 1000);
+}
+
+// Toast
+function toast({ title = "", msg = "", type = "", duration = 3000}) {
+    const main = document.getElementById('toast');
+    if (main) {
+        const toast = document.createElement("div");
+        const autoRemoveId = setTimeout(() => {
+            main.removeChild(toast);
+        }, duration + 1000);
+
+        toast.onclick = (e) => {
+            if (e.target.closest('.toast__close')) {
+                main.removeChild(toast);
+                clearTimeout(autoRemoveId);
+            }
+        };
+
+        const icons = {
+            success: 'uil uil-check-circle',
+            error: 'uil uil-exclamation-triangle'
+        };
+
+        icon = icons[type];
+        const delay = (duration / 1000).toFixed(2);
+
+        toast.classList.add('toast', `toast--${type}`);
+        toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
+        toast.innerHTML = `
+            <div class="toast__icon">
+                <i class="${icon}"></i>
+            </div>
+            <div class="toast__body">
+                <h3 class="toast__title">${title}</h3>
+                <p class="toast__msg">${msg}</p>
+            </div>
+            <div class="toast__close">
+                <i class="uil uil-times"></i>
+            </div>
+        `;
+        main.appendChild(toast);
+    }
+}
+
+// Tách lấy chữ số
+// Nguồn: http://vncoding.net/2015/10/30/tach-cac-chu-so-thuoc-hang-tram-hang-chuc-hang-don-vi/
+function money(number) {
+    let result = ``; // Nếu là chuỗi thì trán đặt biến là const
+    // Vì Const là một hằng số, vì vậy khi khai báo biến const thì bạn phải gán giá trị cho nó luôn, 
+    // sau này cũng không thể thay đổi giá trị cho biến.
+    // Nguồn: https://freetuts.net/bien-va-khai-bao-bien-trong-javascript-265.html
+    // Ví dụ số 9899999
+    let millions = Math.floor(number / 1000000); // Chia cho 1000000 và làm tròn số ta được 9
+    let hundred_thousand = Math.floor((number % 1000000) / 100000); // Chia lấy phần dư ta được 899999 và tiếp tục chia cho 100000 và làm tròn ta được 8
+    let tens_of_thousands = Math.floor((number % 1000000 % 100000) / 10000); // Tương tự lấy phần dư của hàng trục nghìn rồi chia cho 10000 ta được 9
+    let thousand = Math.floor((number % 1000000 % 100000 % 10000) / 1000); // Lấy phần dư hàng nghìn rồi chia cho 1000
+    let hundreds = Math.floor((number % 1000000 % 100000 % 10000 % 1000) / 100); // Lấy phần dư hàng trăm chia cho 100
+    let tens = Math.floor((number % 1000000 % 100000 % 10000 % 1000 % 100) / 10); // Lấy phần dư của hàng chục chia cho 10
+    let unit = Math.floor(number % 1000000 % 100000 % 10000 % 1000 % 100 % 10); // Lấy phần dư hàng đơn vị
+    if (millions == 0 && hundred_thousand != 0) {
+        result = `${hundred_thousand}${tens_of_thousands}${thousand}.${hundreds}${tens}${unit}`;
+    } else if (millions == 0 && hundred_thousand == 0) {
+        result = `${tens_of_thousands}${thousand}.${hundreds}${tens}${unit}`;
+    } else {
+        result = `${millions}.${hundred_thousand}${tens_of_thousands}${thousand}.${hundreds}${tens}${unit}`;
+    }
+    return result;
+}
